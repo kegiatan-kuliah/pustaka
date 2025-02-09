136977c36cf1399318cf45866bce22abc2e93aaf
Ari P <ariteknologi@gmail.com>
2025-02-09 17:13:24 +0700

    add auth function

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -37,15 +37,14 @@
           <div class="navbar-nav flex-row order-md-last">
             <div class="nav-item dropdown">
               <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
-                <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)"></span>
                 <div class="d-none d-xl-block ps-2">
-                  <div>Paweł Kuna</div>
-                  <div class="mt-1 small text-secondary">UI Designer</div>
+                  <div>{{ Auth::user()->name }}</div>
+                  <div class="mt-1 small text-secondary">{{ Auth::user()->role }}</div>
                 </div>
               </a>
               <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <a href="./profile.html" class="dropdown-item">Profile</a>
-                <a href="./sign-in.html" class="dropdown-item">Logout</a>
+                <!-- <a href="./profile.html" class="dropdown-item">Profile</a> -->
+                <a href="{{ route('auth.logout') }}" class="dropdown-item">Logout</a>
               </div>
             </div>
           </div>
--- routes/web.php
+++ routes/web.php
@@ -10,87 +10,98 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\BorrowController;
 use App\Http\Controllers\ReturnController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('layouts.master');
+
+Route::controller(AuthController::class)->prefix('auth')->name('auth.')->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::get('/logout', 'logout')->name('logout');
+    Route::post('/login', 'login')->name('login');
 });
 
-Route::controller(AuthorController::class)->prefix('author')->name('author.')->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/new', 'new')->name('new');
-    Route::get('/{id}', 'edit')->name('edit');
-    Route::post('/', 'store')->name('store');
-    Route::put('/', 'update')->name('update');
-    Route::get('/destroy/{id}', 'destroy')->name('destroy');
-});
-
-Route::controller(PublisherController::class)->prefix('publisher')->name('publisher.')->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/new', 'new')->name('new');
-    Route::get('/{id}', 'edit')->name('edit');
-    Route::post('/', 'store')->name('store');
-    Route::put('/', 'update')->name('update');
-    Route::get('/destroy/{id}', 'destroy')->name('destroy');
-});
-
-Route::controller(BookCategoryController::class)->prefix('book-category')->name('book_category.')->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/new', 'new')->name('new');
-    Route::get('/{id}', 'edit')->name('edit');
-    Route::post('/', 'store')->name('store');
-    Route::put('/', 'update')->name('update');
-    Route::get('/destroy/{id}', 'destroy')->name('destroy');
-});
-
-Route::controller(BookLocationController::class)->prefix('book-location')->name('book_location.')->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/new', 'new')->name('new');
-    Route::get('/{id}', 'edit')->name('edit');
-    Route::post('/', 'store')->name('store');
-    Route::put('/', 'update')->name('update');
-    Route::get('/destroy/{id}', 'destroy')->name('destroy');
-});
-
-Route::controller(UserController::class)->prefix('user')->name('user.')->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/new', 'new')->name('new');
-    Route::get('/{id}', 'edit')->name('edit');
-    Route::post('/', 'store')->name('store');
-    Route::put('/', 'update')->name('update');
-    Route::get('/destroy/{id}', 'destroy')->name('destroy');
-});
-
-Route::controller(BookController::class)->prefix('book')->name('book.')->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/new', 'new')->name('new');
-    Route::get('/{id}', 'edit')->name('edit');
-    Route::get('/detail/{id}', 'detail')->name('detail');
-    Route::post('/', 'store')->name('store');
-    Route::post('/update', 'update')->name('update');
-    Route::get('/destroy/{id}', 'destroy')->name('destroy');
-});
-
-Route::controller(MemberController::class)->prefix('member')->name('member.')->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/new', 'new')->name('new');
-    Route::get('/{id}', 'edit')->name('edit');
-    Route::post('/', 'store')->name('store');
-    Route::put('/', 'update')->name('update');
-    Route::get('/destroy/{id}', 'destroy')->name('destroy');
-});
-
-Route::controller(BorrowController::class)->prefix('borrow')->name('borrow.')->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/new', 'new')->name('new');
-    Route::get('/{id}', 'edit')->name('edit');
-    Route::post('/', 'store')->name('store');
-    Route::put('/', 'update')->name('update');
-});
-
-Route::controller(ReturnController::class)->prefix('return')->name('return.')->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/new', 'new')->name('new');
-    Route::get('/{id}', 'edit')->name('edit');
-    Route::post('/', 'store')->name('store');
-    Route::put('/', 'update')->name('update');
+Route::middleware(['auth'])->group(function () {
+    Route::controller(DashboardController::class)->name('dashboard.')->group(function () {
+        Route::get('/', 'index')->name('index');
+    });
+    
+    Route::controller(AuthorController::class)->prefix('author')->name('author.')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/new', 'new')->name('new');
+        Route::get('/{id}', 'edit')->name('edit');
+        Route::post('/', 'store')->name('store');
+        Route::put('/', 'update')->name('update');
+        Route::get('/destroy/{id}', 'destroy')->name('destroy');
+    });
+    
+    Route::controller(PublisherController::class)->prefix('publisher')->name('publisher.')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/new', 'new')->name('new');
+        Route::get('/{id}', 'edit')->name('edit');
+        Route::post('/', 'store')->name('store');
+        Route::put('/', 'update')->name('update');
+        Route::get('/destroy/{id}', 'destroy')->name('destroy');
+    });
+    
+    Route::controller(BookCategoryController::class)->prefix('book-category')->name('book_category.')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/new', 'new')->name('new');
+        Route::get('/{id}', 'edit')->name('edit');
+        Route::post('/', 'store')->name('store');
+        Route::put('/', 'update')->name('update');
+        Route::get('/destroy/{id}', 'destroy')->name('destroy');
+    });
+    
+    Route::controller(BookLocationController::class)->prefix('book-location')->name('book_location.')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/new', 'new')->name('new');
+        Route::get('/{id}', 'edit')->name('edit');
+        Route::post('/', 'store')->name('store');
+        Route::put('/', 'update')->name('update');
+        Route::get('/destroy/{id}', 'destroy')->name('destroy');
+    });
+    
+    Route::controller(UserController::class)->prefix('user')->name('user.')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/new', 'new')->name('new');
+        Route::get('/{id}', 'edit')->name('edit');
+        Route::post('/', 'store')->name('store');
+        Route::put('/', 'update')->name('update');
+        Route::get('/destroy/{id}', 'destroy')->name('destroy');
+    });
+    
+    Route::controller(BookController::class)->prefix('book')->name('book.')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/new', 'new')->name('new');
+        Route::get('/{id}', 'edit')->name('edit');
+        Route::get('/detail/{id}', 'detail')->name('detail');
+        Route::post('/', 'store')->name('store');
+        Route::post('/update', 'update')->name('update');
+        Route::get('/destroy/{id}', 'destroy')->name('destroy');
+    });
+    
+    Route::controller(MemberController::class)->prefix('member')->name('member.')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/new', 'new')->name('new');
+        Route::get('/{id}', 'edit')->name('edit');
+        Route::post('/', 'store')->name('store');
+        Route::put('/', 'update')->name('update');
+        Route::get('/destroy/{id}', 'destroy')->name('destroy');
+    });
+    
+    Route::controller(BorrowController::class)->prefix('borrow')->name('borrow.')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/new', 'new')->name('new');
+        Route::get('/{id}', 'edit')->name('edit');
+        Route::post('/', 'store')->name('store');
+        Route::put('/', 'update')->name('update');
+    });
+    
+    Route::controller(ReturnController::class)->prefix('return')->name('return.')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/new', 'new')->name('new');
+        Route::get('/{id}', 'edit')->name('edit');
+        Route::post('/', 'store')->name('store');
+        Route::put('/', 'update')->name('update');
+    });
 });
